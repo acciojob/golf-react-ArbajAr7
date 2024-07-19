e96b1e4b6dc21efbb9b3ddc97ac4aa4a94f23355
ArbajAr7 <arbajrmacr786@gmail.com>
2024-07-20 00:14:15 +0530

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,59 +1,12 @@
-import React, { Component, useState } from "react";
-import '../styles/App.css';
+import React from 'react';
+import GolfGame from './GolfGame';
 
-class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            renderBall: false,
-            posi : 0,
-            ballPosition: { marginLeft: '0px' }
-        };
-        this.renderChoice = this.renderBallOrButton.bind(this)
-        this.buttonClickHandler = this.buttonClickHandler.bind(this)
-        this.handleKeyDown = this.handleKeyDown.bind(this)
-    };
-
-    handleKeyDown(event) {
-        if (event.key === "ArrowRight") {
-            this.setState((prevState) => {
-                const newLeft = (parseInt(prevState.ballPosition.marginLeft, 10) || 0) + 5 + "px";
-                console.log("previouspadding: ",prevState.ballPosition.marginLeft);
-                console.log("newleft: ",newLeft);
-                return { ballPosition: { marginLeft: newLeft } };
-            });
-        }
-    }
-
-
-    buttonClickHandler() {
-        this.setState({ renderBall: true });
-   
-   }
-    renderBallOrButton() {
-		if (this.state.renderBall) {
-		    return <div className="ball" style={this.state.ballPosition}></div>
-		} else {
-		    return <button className="start" onClick={this.buttonClickHandler} >Start</button>
-		}
-    }
-
-    // bind ArrowRight keydown event
-    componentDidMount() {
-        document.addEventListener("keydown", this.handleKeyDown);
-    }
-    // componentWillUnmount() {
-    //     document.removeEventListener("keydown", this.handleKeyDown);
-    // }
-
-    render() {
-        return (
-            <div className="playground">
-                {this.renderBallOrButton()}
-            </div>
-        )
-    }
-}
-
+const App = () => {
+  return (
+    <div>
+      <GolfGame />
+    </div>
+  );
+};
 
 export default App;
